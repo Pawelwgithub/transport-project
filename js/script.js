16e09e4649b927a11dbc1164c7dea27bad091d2b
Paweł Walkowski <walkowski.pawel@wp.pl>
2021-05-31 20:54:26 +0200

    Add html, styles and script JS for media menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
+/* Media menu */
+
+const icon = document.getElementById('icon');
+const icon1 = document.getElementById('a');
+const icon2 = document.getElementById('b');
+const icon3 = document.getElementById('c');
+const menu = document.getElementById('menu');
+const color = document.getElementById('color');
+
+icon.addEventListener('click', function() {
+  icon1.classList.toggle('a');
+  icon2.classList.toggle('c');
+  icon3.classList.toggle('b');
+  menu.classList.toggle('show');
+  color.classList.toggle('slide');
+});
+
 /* Scroll to top */
 
 $(function() {
 
-  var icon = $('.scroll-to-top-icon i');
+  const icon = $('.scroll-to-top-icon i');
 
   $(window).scroll(function() {
     if ($(window).scrollTop() > 90) {
